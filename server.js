5ce9d06b607f0c538804b6e48a1eaddc14964b86
Amy Trice <amylizt@gmail.com>
2026-06-16 20:42:48 -0400

    deploymnent fixes again 4

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const server = app.listen(PORT, () => {
     console.log(`🚀 Chat backend server initialized and listening on port ${PORT}`);
 });
 
+//test comment
 process.on('SIGTERM', () => {
     console.log('SIGTERM signal received: closing HTTP server safely');
     server.close(() => {
